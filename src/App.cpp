d5bc2211a7574e44523a815e79044ec0942de2fb
Kacper Kasper <kacperkasper@gmail.com>
2017-01-01 17:09:52 +0100

    Change license to MIT.

--- src/App.cpp
+++ src/App.cpp
@@ -1,21 +1,6 @@
 /*
- * Koder is a code editor for Haiku based on Scintilla.
- *
- * Copyright (C) 2014-2015 Kacper Kasper <kacperkasper@gmail.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Copyright 2014-2017 Kacper Kasper <kacperkasper@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
  */
 
 #include "App.h"
@@ -108,7 +93,7 @@ App::AboutRequested()
 	window->AddDescription(
 		B_TRANSLATE("Code editor for Haiku based on Scintilla editing component."));
 	window->AddSpecialThanks(specialThanks);
-	window->AddExtraInfo(B_TRANSLATE("Distributed on GPL v3 license terms."));
+	window->AddExtraInfo(B_TRANSLATE("Distributed on MIT license terms."));
 	window->Show();
 }
 
