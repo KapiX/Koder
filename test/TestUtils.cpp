a40fcfa9a3e4b9a65be41d92df0e22ccd88e4ee6
Kacper Kasper <kacperkasper@gmail.com>
2019-09-18 16:14:36 +0200

    Make unit tests less verbose

--- test/TestUtils.cpp
+++ test/TestUtils.cpp
@@ -11,116 +11,83 @@
 // GetFileName
 
 TEST(GetFileNameTest, HandlesNoExtension) {
-	const std::string name = "test";
-	const std::string result = GetFileName(name);
-	const std::string expected = "test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName("test");
+	ASSERT_EQ(result, "test");
 }
 
 TEST(GetFileNameTest, HandlesDotFile) {
-	// FIXME: that's wrong
-	const std::string name = ".test";
-	const std::string result = GetFileName(name);
-	const std::string expected = ".test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName(".test");
+	ASSERT_EQ(result, ".test");
 }
 
 TEST(GetFileNameTest, HandlesDotFileWithExtension) {
-	const std::string name = ".test.second";
-	const std::string result = GetFileName(name);
-	const std::string expected = ".test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName(".test.second");
+	ASSERT_EQ(result, ".test");
 }
 
 TEST(GetFileNameTest, HandlesDotFileWithEmptyExtension) {
-	const std::string name = ".test.";
-	const std::string result = GetFileName(name);
-	const std::string expected = ".test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName(".test.");
+	ASSERT_EQ(result, ".test");
 }
 
 TEST(GetFileNameTest, HandlesFileWithEmptyExtension) {
-	const std::string name = "test.";
-	const std::string result = GetFileName(name);
-	const std::string expected = "test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName("test.");
+	ASSERT_EQ(result, "test");
 }
 
 TEST(GetFileNameTest, HandlesFileWithExtension) {
-	const std::string name = "test.txt";
-	const std::string result = GetFileName(name);
-	const std::string expected = "test";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName("test.txt");
+	ASSERT_EQ(result, "test");
 }
 
 TEST(GetFileNameTest, HandlesFileWithLongerExtension) {
-	const std::string name = "testing.extension";
-	const std::string result = GetFileName(name);
-	const std::string expected = "testing";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName("testing.extension");
+	ASSERT_EQ(result, "testing");
 }
 
 TEST(GetFileNameTest, HandlesFileWithTwoExtensions) {
-	const std::string name = "testing.long.extension";
-	const std::string result = GetFileName(name);
-	const std::string expected = "testing.long";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileName("testing.long.extension");
+	ASSERT_EQ(result, "testing.long");
 }
 
 // GetFileExtension
 
 TEST(GetFileExtensionTest, HandlesNoExtension) {
-	const std::string name = "test";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension("test");
+	ASSERT_EQ(result, "");
 }
 
 TEST(GetFileExtensionTest, HandlesDotFile) {
-	const std::string name = ".test";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension(".test");
+	ASSERT_EQ(result, "");
 }
 
 TEST(GetFileExtensionTest, HandlesDotFileWithExtension) {
-	const std::string name = ".test.second";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "second";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension(".test.second");
+	ASSERT_EQ(result, "second");
 }
 
 TEST(GetFileExtensionTest, HandlesDotFileWithEmptyExtension) {
-	const std::string name = ".test.";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension(".test.");
+	ASSERT_EQ(result, "");
 }
 
 TEST(GetFileExtensionTest, HandlesFileWithEmptyExtension) {
-	const std::string name = "test.";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension("test.");
+	ASSERT_EQ(result, "");
 }
 
 TEST(GetFileExtensionTest, HandlesFileWithExtension) {
-	const std::string name = "test.txt";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "txt";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension("test.txt");
+	ASSERT_EQ(result, "txt");
 }
 
 TEST(GetFileExtensionTest, HandlesFileWithLongerExtension) {
-	const std::string name = "testing.extension";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "extension";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension("testing.extension");
+	ASSERT_EQ(result, "extension");
 }
 
 TEST(GetFileExtensionTest, HandlesFileWithTwoExtensions) {
-	const std::string name = "testing.long.extension";
-	const std::string result = GetFileExtension(name);
-	const std::string expected = "extension";
-	ASSERT_EQ(result, expected);
+	const std::string result = GetFileExtension("testing.long.extension");
+	ASSERT_EQ(result, "extension");
 }
